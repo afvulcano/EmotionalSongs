7437b33d48ec942d232b659e9c3cc7ab0fd4a838
jackpav02 <pavianogiacomo@gmail.com>
2022-08-22 10:55:40 +0200

    Update

--- EmotionalSongs/src/main/java/module-info.java
+++ EmotionalSongs/src/main/java/module-info.java
@@ -1,9 +1,7 @@
 module com.esongs.emotionalsongs {
     requires javafx.controls;
     requires javafx.fxml;
-    requires org.kordamp.iconli.core;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.ikonli.fontawesome;
+
 
     opens com.esongs.emotionalsongs to javafx.fxml;
     exports com.esongs.emotionalsongs;
